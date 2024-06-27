1832d629d27762e72ec60862158eb7c466093c0b
2IB2 <i.bouafouss@gmail.com>
2024-06-27 09:23:17 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - printing lines diagonal
- * @n: input variable
+ * print_diagonal - prints a diagonal line of '\' characters
+ * @n: number of times the character '\' should be printed
  *
  * Return: void
  */
@@ -12,14 +12,19 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');
-	for (i = 0; i < n; j++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('\n');
+	_putchar('\n');  // Print a newline for non-positive n
+	return;  // Return from the function if n is 0 or negative
+	}
+
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < i; j++)
+	{
+		_putchar(' ');  // Print j spaces before the backslash
+	}
+	_putchar('\\');  // Print the backslash at the end of the line
+	_putchar('\n');  // Move to the next line
 	}
 }
+
