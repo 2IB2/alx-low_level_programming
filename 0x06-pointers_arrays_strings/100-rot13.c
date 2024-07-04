223f55c848881338219b5ff58da65818e4bd9a37
2IB2 <i.bouafouss@gmail.com>
2024-07-04 10:39:33 -0700

    8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,8 +9,8 @@
 
 char *rot13(char *s)
 {
-	int i, j;
-
+	int i;
+	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
