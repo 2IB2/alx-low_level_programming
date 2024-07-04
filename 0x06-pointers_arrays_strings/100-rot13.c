ba89eb2291352bb4f27118659c41ca37de171930
2IB2 <i.bouafouss@gmail.com>
2024-07-04 10:29:31 -0700

    8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string params
- * Return: *s
+ * rot13 - Encode a string using ROT13 substitution cipher.
+ * @s: Pointer to the string to be encoded.
+ * Return: Pointer to the encoded string `s`.
  */
 
 char *rot13(char *s)
 {
-	int i;
-	int j;
+	int i, j;
+
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 57; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
@@ -24,5 +25,5 @@ char *rot13(char *s)
 			}
 		}
 	}
-	return (s);
+	return s;
 }
