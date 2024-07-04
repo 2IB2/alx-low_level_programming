9f531abd801f6068ae913af8596fc2c9035a4e48
2IB2 <i.bouafouss@gmail.com>
2024-07-04 08:14:06 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,6 +6,7 @@
  *
  * Return: pointer to s.
  */
+
 char *cap_string(char *s)
 {
 	int string_count;
@@ -18,11 +19,7 @@ char *cap_string(char *s)
 		{
 			s[0] = s[0] - 32;
 		}
-		if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n' 
-			|| s[string_count] == ',' || s[string_count] == ';' || s[string_count] ==  '.'
-			|| s[string_count] == '.' || s[string_count] == '!' || s[string_count] == '?'
-			|| s[string_count] == '"' || s[string_count] == '(' || s[string_count] == ')'
-			|| s[string_count] == '{' || s[string_count] == '}')
+		if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n' || s[string_count] == ',' || s[string_count] == ';' || s[string_count] ==  '.' || s[string_count] == '.' || s[string_count] == '!' || s[string_count] == '?' || s[string_count] == '"' || s[string_count] == '(' || s[string_count] == ')' || s[string_count] == '{' || s[string_count] == '}')
 		{
 			if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
 			{
